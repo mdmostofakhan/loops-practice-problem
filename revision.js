3d7fe90faa2b9bb0e93e55e2c809a13c1fe8d990
mostofa khan <mdmostofakhan1972@gmail.com>
2023-01-30 23:47:23 +0600

    first js

---
+++ revision.js
@@ -0,0 +1,33 @@
+// string type variable
+var bottle = 'water';
+var bottleColor = 'yellow';
+// Number type variable
+var sunglassPrice = 120;
+var prnColor = 'blue';
+//  beloop type variable
+var isExpensive = true
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+var items = tableItems[3];
+tableItems[2] = 'notebook';
+// console.log(items)
+
+var penIndex = tableItems.indexOf('pen');
+// console.log(penIndex);
+// push pop
+
+// conditionals
+
+if (tableItems.length > 3) {
+    console.log(tableItems);
+}
+
+// var chickenPrice = 3330;
+// var myMoney = 4990;
+
+// if (chickenPrice > myMoney) {
+//     console.log('I will eat chiken');
+// }
+// else {
+//     console.log('I will eat potato');
+// }
---
+++ while.js
@@ -0,0 +1,12 @@
+// if (condition){
+    
+// }
+
+var rostGiven = 0;
+while (rostGiven < 7) {
+    console.log('Roast, please !!')
+    // rostGiven = rostGiven + 1;
+    // rostGiven += 1;
+    console.log(rostGiven);
+    rostGiven++;
+    }
