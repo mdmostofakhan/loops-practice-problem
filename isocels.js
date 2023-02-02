7f3436488e4c3ced02b35b0df8d2cab725c5574d
mostofa khan <mdmostofakhan1972@gmail.com>
2023-02-02 20:14:36 +0600

    question

--- isocels.js
+++ isocels.js
@@ -8,7 +8,7 @@ if (side1 == side2 || side1 == side3 || side2 == side3){
 
 var i = 0;
 for (i=1; i<= 5; i++);
-// console.log(i);
+console.log(i);
 
 for (var i = 10; i < 10; i++){
     // console.log(i);
--- question.js
+++ question.js
@@ -1,4 +1,5 @@
-// VARIABLE
+//        VARIABLE        //
+// -----------------------//
 // 1. What is Javascript ?
 // 2. How does js Works ?
 // 3. What is Variable?
@@ -9,5 +10,64 @@
 // 8. math Opetation +, -, *, /, %
 // 9. short hand: +=, -=, *=, /=
 // 10. ++, --
-// 11. parseInt, parsefloat 
+// 11. parseInt, parsefloat
 // roFixes
+
+
+/*
+--------------------
+        ARRay
+-------------------
+1. what is the purpose 
+2. How to declare an array in js
+3. Number of elements in an array
+4. What is index?
+5. Find the value if an ekement by index
+6. get the index of an element by the value
+7.change an element by index
+8.what does it mean when you get undefined while getting the valie the value of an element by index
+9. How can you add an element to an array
+10. How can you rrmove the last element from array
+11. Add an element at the first position os an array
+12. Remove the first elemtnt of an array
+*/
+
+
+/*
+----------------------------
+        CONDITIOINALS
+----------------------------
+1. meaning of: >, <, >=, <=, ==, !=, ===, !==
+2.Meaning of &&
+3.Meaning of ||
+4. lets say you have x amout of money. if you have more than 80,000, then you will by a mac
+if more than 60,000 then you will by gaming laptop
+if you more than 40,000 than you will by lenovo Yoga
+if you have more than 20, 000 than you will by an used laptop
+otherwide, you will use you mobile phone
+*/
+
+/*
+------------------------
+        Loop
+-----------------------
+1. Display "Ajke amar mon valo nei" for 39 tims
+2. Show all even number from 412 to 456
+3. display numvers between 58 to 98
+4. Show all odd numvers 581 to 623
+5. Difference between while loop and for loop
+6. Declare an array for all the topice that you have learned last few days 
+display then as output
+7. create an array for all the mobile phones. Display all the elements of the 
+by using a while loop
+8. Run a loop from 30 to 86. this loop will stop if the values get higher the
+9. Write the price of the books that you have. Display the\
+ prices if the prices is lower than 200
+
+*/
+
+
+var num = 700;
+var num2 = 23;
+var totalMark = num + num2;
+console.log(totalMark);
